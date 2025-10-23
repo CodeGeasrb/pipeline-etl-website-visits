bdad045196c39fa36b2b858617b49474930211c9
Rigo <ririba@outlook.es>
2025-10-23 10:36:29 -0600

    Add python-dotenv dependency and implement SFTP connection and logging functionality
    - Added python-dotenv to manage environment variables for SFTP and MySQL configurations.
    - Implemented sftp_connection context manager for secure SFTP connections.
    - Created setup_logger function for logging extraction processes.
    - Developed extraction task to download files from SFTP server and log the process.

--- taks/extract.py
+++ taks/extract.py
@@ -1,7 +1,28 @@
+from utils.utils import setup_logger, sftp_connection
+from prefect import task
+from pathlib import Path
 
 
-
-
+# Tarea de extracción de datos
+@task(name="Extracción de datos", retries=2, retry_delay_seconds=60)
+def extraction(filename: str) -> Path:
+    """Descarga un archivo desde el servidor de inicio a un directorio temporal en el servidor del ETL"""
+    # inicializar un archivo log
+    logger = setup_logger(filename)
+    # crear logging de inicio
+    logger.info("Iniciando etapa de extración...")
+    # validar que el directorio temporal existe o crearlo
+    "staging_path".mkdir(parents=True, exist_ok=True)
+    # definir la ruta de origen del archivo
+    remote_path = f"remotedir/{filename}"
+    # definir la ruta completa de descarga del archivo
+    local_path = f"staging_path/{filename}"
+    # descargar el archivo
+    with sftp_connection() as sftp:
+        sftp.get(remote_path, local_path)
+    # loggear fin de tarea
+    logger.info(f"Archivo {filename} descargado a {local_path} con éxito")
+    return local_path
 
 
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,58 @@
 from contextlib import contextmanager
+from dotenv import load_dotenv
+
+import logging
+import datetime
 import paramiko
+import os
 
-""" Conexión SFTP al servidor de inicio """
+# Cargar variables de entorno para la configuración de la conexión SFTP y MySQL
+load_dotenv(dotenv_path="config/.env")
+
 @contextmanager
-def 
+def sftp_connection():
+    # inicializamos los recursos de conexión como None
+    transport = None
+    sftp = None
+    # intentar conexión a servidor sftp
+    try:
+        # se definen las credenciales de conexión
+        port = int(os.getenv("PORT_SFTP"))
+        host = os.getenv("HOST_SFTP")
+        # establece la conexion mediante sftp
+        transport = paramiko.Transport((host, port))
+        transport.connect(username=os.getenv("USER_SFTP"), password=os.getenv("PASSWORD_SFTP"))
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        # cedemos control al with
+        yield sftp
+    finally:
+        # cerramos la conexión si está fue establecida
+        if sftp is not None:
+            sftp.close()
+        if transport is not None:
+            transport.close()
+
+""" Setup de la configuración de logger """
+def setup_logger(filename: str) -> logging.Logger:
+    # fecha estandarizada
+    log_date = datetime.now().strftime("%d%m%y")
+    # directorio del log del archivo que será procesado
+    log_dir = "dir"
+    log_dir.mkdir(parents=True, exist_ok=True)  # asegurar que el log existe, sino crearlo
+    return logging.getLogger(filename)
 
 
-
-
+""" Configuración del mysql connection string para sqlalchemy """
+def mysql_connection_string() -> str:
+    # leer las credenciales del servidor mysql
+    host = os.getenv("HOST_MYSQL")
+    port = os.getenv("PORT_MYSQL")
+    user = os.getenv("USER_MYSQL")
+    password = os.getenv("PASSWORD_MYSQL")
+    database = os.getenv("DATABASE_MYSQL")
+    # crear el connection string
+    connection_string = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    return connection_string
 
 
 
